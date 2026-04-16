eafd3acd0bc5d26e33965ddf18794a85bed7b2fe
PabloNicolasGarcia <pablonicoryan@gmail.com>
2026-04-16 17:04:02 +0200

    Se crea fichero .yml para realizar test automaticos. También se añade un test de prueba para la clase vehículo.

--- src/main/java/Vehiculo.java
+++ src/main/java/Vehiculo.java
@@ -1,2 +1,7 @@
 public class Vehiculo {
+    public Vehiculo(String marca, String modelo, int velocidadMaxima) {
+        }
+    public int getVelocidadActual() {
+        return -1; // valor incorrecto a propósito
+    }
 }
--- src/test/java/VehiculoTest.java
+++ src/test/java/VehiculoTest.java
@@ -1,8 +1,11 @@
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VehiculoTest {
 
     @Test
-    void ejemplo() {
+    void crearVehiculo() {
+        Vehiculo vehiculo = new Vehiculo("Seat", "Ibiza", 180);
+        assertEquals(0, vehiculo.getVelocidadActual());
     }
 }
